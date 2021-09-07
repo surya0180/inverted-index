21934b0c3939340cb8b7897a2570be7a96885916
surya0180 <teja8551@gmail.com>
2021-09-07 21:08:32 +0530

    Cmpleted step 1

---
+++ README.txt
@@ -0,0 +1,101 @@
+About the Data Structure:-
+
+Delimiters: Used a basic python list to store all the required delimiters in it.
+
+Stop Words: Used a basic python list to store all the required stop words in it.
+
+Vocabulary: Used a list of dictionaries with terms and document id's as key value pairs.
+            Example: [
+                        {
+                            "term": "advocate",
+                            "docId": 103
+                        }
+                    ]
+
+Posting Lists: Used a basic python list to store all the required document ids in it.
+
+Inverted Index: The entire data structure is as follows:
+
+                1.) A dictionary is used to store all the terms as keys.
+                2.) The values for these keys will be another dictionaries.
+                3.) Now these dictionaries contains two key value pairs namely "frequency" and "posting list".
+                4.) Where frequency is the total number of occurences of a word in all documents.
+                5.) Posting list consists of all the document id's in which the word is present.
+
+            Example: {
+                "advocate": {
+                    "frequency": 28,
+                    "posting_list": [103, 105, 112, 118, 196, 225, 268]
+                }
+            }
+
+
+Time Complexity:-
+
+1.) It takes O(k) to iterate through the delimiters where k is the length of the delimiter list which is a constant, 
+therefore we can ignore it. Same goes for the stop words.
+
+2.) All the documents will be iterated which takes O(m) where m is the total number of documents.
+
+3.) For each document the tokenization process will take the order of O(n) where n is the total number of tokens generated.
+
+4.) After all the documents are tokenized, the total time complexity would be in the order of O(m)*O(n) = O(m*n),
+where m is the total number of documents and n is the total number of tokens on an average generated per file.
+
+5.) Sorting the tokens and generating the vocabulary will take the order of O(m*n) where n is the number of tokens generated per 
+document and m is the total number of documents.
+
+6.) Generating the posting lists from the vocabulary will take another O(m*n) where n is the number of tokens generated per 
+document and m is the total number of documents.
+
+7.) If we sum up all the complexities we got, we will get O(m*n)
+
+
+
+Space Complexity:-
+
+We will be storing all the n terms generated from all the documents in the form of a dictionary where the posting list 
+takes the number of documents the particular term occured. So, for all the m documents the space complexity in worst 
+possible case would be O(m*n), where m is the total number of documents stored in the posting list, and n is the 
+total number of terms.
+
+
+In Short:
+
+Time-Complexity:-  O(m*n) where m is the total number of documents and n is the number of tokens generated per document.
+Space-Complexity:- O(m*n) where m is the total number of documents stored in the posting list, and n is the  
+                   total number of terms.
+
+
+
+Some points regarding my implemetation:
+
+1.) I considered all the possible delimiters to be used for tokenization and stored them in an array, same with the
+stop words aswell
+
+2.) Firstly i remove all the delimiters by replacing them simply with white spaces and then remove all the stop words.
+
+3.) Important thing that should be noted here is that the stop words list is not a fixed one in this assignment. 
+After searching the internet, I have chosen the best possible list of stop words which I thought would be sufficient 
+for this assignment. Replacing the stop words will simply change the outcomes of the result completely.
+
+4.) After removing the stop words there are few delimiters which are left, removing them will complete the tokenization 
+process and we can return the generated terms.
+
+5.) Storing all of them in a list of dictionaries will give us the vocabulary.
+
+6.) Passing the vocabulary to the function, we are generating the inverted index with the frequencies and posting 
+lists of each word.
+
+7.) Here the frequencies are the total number of times a word has occured in all the document. For example:
+    if advocate occured: 
+        3 time in doc 1
+        5 time in doc 2
+        8 time in doc 3
+    the frequency of the word "advocate" is 16 for the 3 documents.
+
+Note:-
+
+1.) Please make sure you download the library (termtables [ pip install termtables(windows) ]) before you run the code.
+2.) To print the vocabulary which is stored in the main memory, simply uncomment the code from line (73, 81).
+3.) Please change the path of the dataset in the code before you run it. Same with all the output files and stop words
--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-def tokenizeFile(file, delimiters, stop_words, docId):
-    # Removing all the stop words from the document
-    # for i in stop_words:
-    #     file = str(file).replace(" "+i+" ", ' ')
+import termtables as tt
 
+# Function used to tokenize the file
+def tokenizeFile(file, delimiters, after_delimiters, stop_words, docId):
     # Removing all the delimiters from the document and replacing them with whitespace
     # Because by doing like that, we can split the document very easily.
     for i in delimiters:
         file = str(file).replace(i, ' ')
+    # print(file)
+
+    # Removing all the stop words from the document
+    for i in stop_words:
+        file = str(file).replace(" "+i+" ", ' ')
+    # print(file)
+
+    # Removing the delimiters which must be removed after removing the stop words
+    for i in after_delimiters:
+        file = str(file).replace(i, ' ')
+    # print(file)
 
     # Splitting the document into terms
     step_1 = str(file).split(' ')
@@ -18,15 +28,32 @@ def tokenizeFile(file, delimiters, stop_words, docId):
             finalised_words.append({'term': i.lower(), 'docId': docId})
     return finalised_words # Returning the final tokenized words as a list
 
+# Function used to create the main inverted index
+def prepareInvertedIndex(dictionary):
+    inverted_index = {} # Initialising an empty dictionary
+
+    for i in dictionary: # looping through the vocabulary we created and passed to this function
+
+        if inverted_index.get(i['term']) == None: # Basically checks if the term exists or not
+            inverted_index[i['term']] = {'frequency': 0, 'posting_list': []} # Initialising the dictionary with empty posting list and 0 frequency
+
+        if i['docId'] not in inverted_index[i['term']]['posting_list']: # Checking whether the document id is present or not in the posting list of respective term
+            inverted_index[i['term']]['posting_list'].append(i['docId']) # Appending the document id here
+        inverted_index[i['term']]['frequency'] = inverted_index[i['term']]['frequency'] + 1 # Increasing the frequency of the term
+
+    return inverted_index # Returning the complete inverted index
+
 if __name__ == '__main__':
     # Opening the text files required for the tokenizing ( Stop words and the output of the final tokenized words )
     sw = open('./stop_words.txt', 'r+', encoding='utf8')
     output = open('./output.txt', 'w', encoding='utf8')
 
     # Delimiters used in the tokenized process which includes all the necessary special charachters
-    delimiters = ['.', ',', '“', '”', '-', "’s","'s", "\n", "\t", "'", "’", "‘", '—', '/', 
+    delimiters = ['.', ',', '“', '”', '-', "’s","'s", "\n", "\t", '—', '/', 
                   '(', ')', "!", "&", "~", "@", "#", "$", "%", "^", "*", "_", "₹", '\\',
                   "+", "=", "`", "<", ">", "?", "|", "[", "]", "{", "}", ":", ";", "\xa0"]
+    # Delimiters used to remove after removing the stop words
+    after_delimiters = ["'", "’", "‘"]
 
     stop_words = sw.read().split('\n') # Stop words used for tokenizing the terms
     dictionary = []  # Initialising the universal dictionary of words as an empty list
@@ -34,14 +61,38 @@ if __name__ == '__main__':
     # An iterator which iterates through all the documents and retrieves all the terms and stores them in a dictionary
     for number in range(101, 282):
         document = open('./th-dataset/{}.txt'.format(number), 'r+', encoding='utf8') # Opening the appropriate document w.r.t the iterator
-        tokenized_words = tokenizeFile(document.read().lower(), delimiters, stop_words, number) # Retrieving the terms after the tokeniziation process
+        tokenized_words = tokenizeFile(document.read().lower(), delimiters, after_delimiters, stop_words, number) # Retrieving the terms after the tokeniziation process
         dictionary = dictionary + tokenized_words # Adding the retrieved terms to the universal dictionary
         document.close() # Closing the opened document
     sw.close() # Closing the opened Stop words file
 
     dictionary = sorted(dictionary, key=lambda k: k['term'])  # Arranging the universal dictionary in alphabetical order
+    
+    # Below code will print the vocabulary stored in the main memory.
 
-    # Outputting the universal dictionary to the output.txt
-    for i in dictionary:
-        output.write(i['term']+" "+str(i['docId'])+"\n")
-    output.close() # Closing the opened output file
+    # # Print the Vocabulary in the terminal by uncommenting this code
+    # dictionary_data = []
+    # for i in dictionary:
+    #     dictionary_data.append([i['term'], str(i['docId'])])
+    # print(tt.to_string(
+    #     dictionary_data,
+    #     header = ["Term", "Document ID"],
+    #     style = tt.styles.ascii_thin_double,
+    # ))
+
+    inverted_index = prepareInvertedIndex(dictionary) # Creating the inverted index by passing the vocabulary to the function
+
+    # Generating the table ui to print it the the output.txt. 
+    # Please make sure that you install the necessary library before running this code.
+    tabulate_data = []
+    for i in inverted_index:
+        tabulate_data.append([i , str(inverted_index[i]['frequency']) , " ".join(str(i) for i in inverted_index[i]['posting_list'])])
+    string = tt.to_string(
+        tabulate_data,
+        header=["Term", "Frequency", "Posting List"],
+        style=tt.styles.ascii_thin_double,
+    )
+
+    # Storing the Posting lists( Inverted Index ) in the secondary memory ( Hard drive, SSD )
+    output.write(string)
+    output.close()
